d4dbc418e0ed0595311fdd1572f9b819b9330a11
jax7sec <jaxc0nso1e@gmail.com>
2024-03-08 16:01:16 +0800

    启动脚本

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 删除旧的 yeti 和 yeti-feeds-frontend 目录
+rm -rf yeti
+rm -rf yeti-feeds-frontend
+
+# 克隆最新的代码
+git clone https://github.com/jax7sec/yeti-feeds-frontend.git
+git clone https://github.com/jax7sec/yeti.git
+
+# 构建并启动容器
+docker-compose build --no-cache && docker-compose up
